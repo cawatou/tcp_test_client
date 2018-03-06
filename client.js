9fc6a305fec41fa6ef7e576ce39c4133bb97cebe
cawatou <cawatou@gmail.com>
2018-03-06 18:45:59 +0300

    default

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ var net = require('net');
 var request = process.argv[2];
 var skip = process.argv[3] ? process.argv[3] : '';
 var client = new net.Socket();
+
 client.connect(1337, '185.22.235.182', function() {
 	console.log('Connected');
     
@@ -15,24 +16,28 @@ client.connect(1337, '185.22.235.182', function() {
             client.write(config_request);
             break;
         
-        case 'key':
-            var kеу_initial = {
-                "event": "kеу_initial",
-                "serial_number": "8400503587105c2e08"
-            };
-            kеу_initial = JSON.stringify(kеу_initial);
-            client.write(kеу_initial);
-            break;
-        
-        case 'key_next':
-            var key_next = {
-                "event": "key_next",
+        case 'keys':
+            var keys = {
+                "event": "keys",
                 "serial_number": "8400503587105c2e08",
                 "skip": skip
             };
-            key_next = JSON.stringify(key_next);
-            client.write(key_next);
+            keys = JSON.stringify(keys);
+            client.write(keys);
             break;
+
+        case 'flats':
+            var flat = {
+                "event": "flats",
+                "serial_number": "8400503587105c2e08",
+                "skip": skip
+            };
+            flat = JSON.stringify(flat);
+            client.write(flat);
+            break;
+
+        default:
+            client.write('undefined');
     }
 });
 
